9b4adec5c2ecd11a9bc96e6c97d0ebe1407acfb0
selvyfitriani31 <selvyfiriani@gmail.com>
2021-07-12 19:00:30 +0700

    custom views

--- web.rb
+++ web.rb
@@ -1,11 +1,18 @@
 require 'sinatra'
 
 get '/messages' do
-    "<h1 style=\"background-color:DodgerBlue;\">Hello world</h1>"
+    name = params["name"]
+    color = params["color"] ? params["color"] : "DodgerBlue"
+    erb :messages, locals: {
+        name: name,
+        color: color
+    }
 end 
 
-get '/messages/:name' do
-    name = params['name']
-    color = params['color'] ? params['color'] : 'DodgerBlue' 
-    "<h1 style=\"background-color:#{color};\">Hello #{name}</h1>"
+post 'login' do
+    if params['username'] == 'admin' && params['password'] == 'admin'
+        return 'Logged in!'
+    else
+        redirect '/login'
+    end
 end
