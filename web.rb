518fe1064c11c8b6ee7edec3c13a3227fe6f3875
selvyfitriani31 <selvyfiriani@gmail.com>
2021-07-12 17:38:07 +0700

    parsing param to get

--- web.rb
+++ web.rb
@@ -3,3 +3,8 @@ require 'sinatra'
 get '/messages' do
     "<h1 style=\"background-color:DodgerBlue;\">Hello world</h1>"
 end 
+
+get '/messages/:name' do
+    name = params['name']
+    "<h1 style=\"background-color:DodgerBlue;\">Hello #{name}</h1>"
+end
