3eda993cbdc4d462bf7b1cda107ee5a6422187fb
selvyfitriani31 <selvyfiriani@gmail.com>
2021-07-12 17:45:28 +0700

    parsing parameter

--- web.rb
+++ web.rb
@@ -6,5 +6,6 @@ end
 
 get '/messages/:name' do
     name = params['name']
-    "<h1 style=\"background-color:DodgerBlue;\">Hello #{name}</h1>"
+    color = params['color'] ? params['color'] : 'DodgerBlue' 
+    "<h1 style=\"background-color:#{color};\">Hello #{name}</h1>"
 end
