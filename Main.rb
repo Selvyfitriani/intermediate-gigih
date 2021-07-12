ca14a233658ce02df345177e95d856bf4415a5d5
selvyfitriani31 <selvyfiriani@gmail.com>
2021-07-12 13:57:36 +0700

    do_attack and reduce_point

--- Main.rb
+++ Main.rb
@@ -2,3 +2,5 @@ require_relative "Player"
 
 player1 = Player.new(1, "Jin Sakai", 100, 50)
 player1.to_string()
+player1.reduce_point(100)
+player1.to_string()
--- Player.rb
+++ Player.rb
@@ -6,6 +6,14 @@ class Player
         @attack_damage = attack_damage
     end
 
+    def do_attack(other_user)
+        other_user.reduce_point(attack_damage)
+    end
+
+    def reduce_point(point)
+        @hit_point -= point
+    end
+
     def to_string()
         puts "#{@name} has #{@hit_point} hitpoint and #{@attack_damage} attack damage"
     end
