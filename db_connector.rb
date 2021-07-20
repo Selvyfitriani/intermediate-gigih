7ce1254fc8522808ef77618e8405481b59654a39
selvyfitriani31 <selvyfiriani@gmail.com>
2021-07-20 11:21:07 +0700

    add func to get all categories

--- db_connector.rb
+++ db_connector.rb
@@ -8,6 +8,23 @@ def create_db_client
         :database => "food_oms_db"
     )
 
-    clientgit
+    client
 end
 
+def get_all_categories
+    client = create_db_client()
+    return client.query("SELECT * FROM categories")
+end
+
+def main()
+
+    # print all categories
+    categories = get_all_categories()
+    categories.each do |category|
+        puts(category)
+    end
+
+    
+end
+
+main()
