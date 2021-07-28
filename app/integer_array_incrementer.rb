89c5e96405f82af0514e38e2a9748416b3467c05
Selvy Fitriani <selvyfiriani@gmail.com>
2021-07-28 19:11:08 +0700

    able to return 2 digits from input 1 digit

--- app/integer_array_incrementer.rb
+++ app/integer_array_incrementer.rb
@@ -1,5 +1,14 @@
 class IntegerArrayIncrementer
     def increment(input)
-        [input[0] + 1]
+        output = []
+        if input.length == 1
+            if input[0] < 9
+                output << input[0] + 1
+            else
+                output << 1
+                output << 0
+            end
+        end
+        return output
     end
 end
--- spec/integer_array_increment_spec.rb
+++ spec/integer_array_increment_spec.rb
@@ -21,5 +21,13 @@ RSpec.describe IntegerArrayIncrementer do
             actual_output = @incrementer.increment(input)
             expect(actual_output).to eq(expected_output)
         end
+
+        it 'should return [1, 0] when input is [9]' do
+            input = [9]
+            expected_output = [1, 0]
+
+            actual_output = @incrementer.increment(input)
+            expect(actual_output).to eq(expected_output)
+        end
     end
 end
