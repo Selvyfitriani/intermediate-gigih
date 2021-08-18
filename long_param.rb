faed5ad69dc09d3e4a5e4fbc43777fc69d836fdb
Selvy Fitriani <selvyfiriani@gmail.com>
2021-08-18 18:56:49 +0700

    grouping previous params into class

--- long_param.rb
+++ long_param.rb
@@ -9,3 +9,12 @@ end
 def total_net_income_in(daterange, reports)
 
 end
+
+class DateRange
+    attr_reader :start_date, :end_date
+
+    def initialize(start_date, end_date)
+        @start_date = start_date
+        @end_date = end_date
+    end
+end
