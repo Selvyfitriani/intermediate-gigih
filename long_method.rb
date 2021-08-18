ef496b9d724c92c27b6408997639768894a367c3
Selvy Fitriani <selvyfiriani@gmail.com>
2021-08-18 17:34:17 +0700

    break function

--- long_method.rb
+++ long_method.rb
@@ -1,10 +1,21 @@
 def get_total_net_income(reports)
     total_net_income = 0
+
     reports.each do |report|
-        gross_income = report.income - report.expense
-        net_income = report.tax.income_tax(gross_income)
+        net_income = get_net_income(report)
         total_net_income = total_net_income + net_income
     end
+    
     return total_net_income
 end
 
+def get_net_income(report)
+    gross_income = get_gross_income(report)
+    net_income = report.tax.income_tax(gross_income)
+    net_income
+end
+
+def get_gross_income(report)
+    gross_income = report.income - report.expense
+    gross_income
+end
