3c47bfacc1ae4b86431c29bf269ddf2f2017615c
Selvy Fitriani <selvyfiriani@gmail.com>
2021-08-18 18:38:30 +0700

    add new file: long param

---
+++ long_param.rb
@@ -0,0 +1,11 @@
+def total_income_in(start_date, end_date, reports)
+
+end
+
+def total_expense_in(start_date, end_date, reports)
+
+end
+
+def total_net_income_in(start_date, end_date, reports)
+
+end
