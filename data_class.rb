1719f0e113d3d3a863adf4e597cce8749eb7bb7c
Selvy Fitriani <selvyfiriani@gmail.com>
2021-08-20 17:56:31 +0700

    create new behaviour in data class

--- data_class.rb
+++ data_class.rb
@@ -1,13 +1,15 @@
 def average_income_in(date_range)
-    total_days = (date_range.end_date - date_range.start_date).to_i
+    total_days = date_range.get_total_days
     total_income / total_days
 end
 
 class DateRange
-    attr_reader :start_date, :end_date
-
     def initialize(start_date, end_date)
         @start_date = start_date
         @end_date = end_date
+    end
+
+    def get_total_days
+        (@end_date - @start_date).to_i
     end
 end
