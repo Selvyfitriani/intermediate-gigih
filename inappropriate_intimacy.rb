1201d1e2766add0bbca9eed7c94cefff917780df
Selvy Fitriani <selvyfiriani@gmail.com>
2021-08-20 17:37:59 +0700

    hide delegate approach

--- inappropriate_intimacy.rb
+++ inappropriate_intimacy.rb
@@ -1,12 +1,16 @@
 class Report
     attr_reader :income, :expense, :tax, :date
 
-    def initialize(income, expense, tax, date)
+    def initialize(income, expense, type, date)
         @income = income
         @expense = expense
         @tax = Tax.new(type)
         @date = date
     end
+
+    def income_tax
+        @tax.income_tax(@income)
+    end
 end
 
-income_tax = report.tax.income_tax(gross_income)
+income_tax = report.income_tax
